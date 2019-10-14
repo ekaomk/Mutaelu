07df999e1c8c87b00b8c77f14b76d78e7437beee
Ekachai Omkaew <ekaomk@gmail.com>
2019-10-14 14:16:34 +0700

    update

---
+++ apps/holy_place/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+def index(request):
+    template = loader.get_template('index.html') # getting our template  
+    return HttpResponse(template.render())       # rendering the template in HttpResponse  
