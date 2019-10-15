fb5437d418cd75a0ff0cd3d8b5b4bccfd471b00e
Ekachai Omkaew <ekaomk@gmail.com>
2019-10-15 15:24:00 +0700

    Auto stash before revert of "fix"
    fix bug

--- apps/holy_place/helper/file_helper.py
+++ apps/holy_place/helper/file_helper.py
@@ -1,16 +1,17 @@
 import os
 from uuid import uuid4
+from django.utils.deconstruct import deconstructible
 
 # rename file with uuid
-def path_and_rename(path):
-    def wrapper(instance, filename):
+@deconstructible
+class PathAndRename(object):
+
+    def __init__(self, sub_path):
+        self.path = sub_path
+
+    def __call__(self, instance, filename):
         ext = filename.split('.')[-1]
-        # get filename
-        if instance.pk:
-            filename = '{}.{}'.format(instance.pk, ext)
-        else:
-            # set filename as random string
-            filename = '{}.{}'.format(uuid4().hex, ext)
+        # set filename as random string
+        filename = '{}.{}'.format(uuid4().hex, ext)
         # return the whole path to the file
-        return os.path.join(path, filename)
-    return wrapper
+        return os.path.join(self.path, filename)
