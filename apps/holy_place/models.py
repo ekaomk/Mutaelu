21556ae33363e95c1556a3c9ac9341e38ca81511
Ekachai Omkaew <ekaomk@gmail.com>
2019-10-15 15:17:03 +0700

    update

--- apps/holy_place/models.py
+++ apps/holy_place/models.py
@@ -1,7 +1,22 @@
 from django.db import models
 from django.conf import settings
 
-from .helper import file_helper
+import os
+from uuid import uuid4
+
+# rename file with uuid
+def path_and_rename(path):
+    def wrapper(instance, filename):
+        ext = filename.split('.')[-1]
+        # get filename
+        if instance.pk:
+            filename = '{}.{}'.format(instance.pk, ext)
+        else:
+            # set filename as random string
+            filename = '{}.{}'.format(uuid4().hex, ext)
+        # return the whole path to the file
+        return os.path.join(path, filename)
+    return wrapper
 
 # Create your models here.
 class Category(models.Model):
